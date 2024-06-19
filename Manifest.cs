f3f7921f8d939617db9b1aa1cb8e3692b9cbce7d
milosh-96 <milosjovanovic042@gmail.com>
2024-06-19 04:28:20 +0200

    ..

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@
     Name = "Taxonomy Posts",
     Author = "Miloš Jovanović - M96 Dev",
     Website = "https://dbmilos.com",
-    Version = "0.9.0",
+    Version = "0.9.1",
     Description = "Provides Taxonomy Posts widget that lists content items attached to a taxonomy item such as category or tag.",
     Dependencies = new[] { "OrchardCore.Contents","OrchardCore.Taxonomies","OrchardCore.Layers" },
     Category = "Content Management"
